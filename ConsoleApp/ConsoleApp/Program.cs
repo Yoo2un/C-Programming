881b24926e99d29026b9ce68adfaf88c8e6908d4
이윤정 <saymeyjlee@gmail.com>
2023-03-14 19:43:59 +0900

    C#설정

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        //class가 단위
+        static void Main(string[] args)
+        {
+            /*//Console.Write("OwO");
+            Console.WriteLine("Hello, World!");
+
+            byte data = 10;
+            bool TF = false;
+            char alphabet = 'A';
+            int value = 100;
+            float pi = 3.14f;
+            double tmp = 20.65;
+
+            Console.WriteLine("alphabet: " + alphabet);
+            Console.WriteLine("value: " + value);
+            Console.WriteLine("pi: " + pi);*/
+
+            //참조 타입
+            // string
+            // class
+            // object
+            // delegate
+            
+            /*int[] array = new int[4];
+            array[0] = 100;
+            array[1] = 200;
+            array[2] = 300;
+            array[3] = 400;
+            
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("array[" + i + "]: " + array[i]);
+            }
+
+            foreach(int element in array)
+            {
+                Console.WriteLine(element);
+            }*/
+        }
+    }
+}
