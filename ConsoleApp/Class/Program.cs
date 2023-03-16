02257f205b930e9fad2b8d2d45ee39ecf5d925b2
이윤정 <saymeyjlee@gmail.com>
2023-03-16 19:50:27 +0900

    class/get/set

---
+++ ConsoleApp/Class/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Class
+{
+    #region get/set class
+    //class Dog//12byte
+    //{
+    //    private int age;
+
+    //    public int Age
+    //    {
+    //        get 
+    //        {
+    //            return age;
+    //        }
+    //        set 
+    //        {
+    //            if(value >= 18)
+    //            {
+    //                value = 18;
+    //            }
+    //            age = value; 
+    //        }
+    //    }
+    //    protected char grade; 
+    //    private float health;
+
+    //    //기본 접근지정자: private
+
+    //}
+    //class Shiba : Dog //상속 받아서 12byte
+    //{
+    //    public void Info()
+    //    {
+    //        grade = 'A';
+    //        Console.WriteLine("grade: " + grade);
+    //    }
+    //}
+    #endregion
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 클래스
+            //Dog dog = new Dog(); //힙에 할당
+            //dog.Age = 20;
+            //Console.WriteLine("강아지 나이: " + dog.Age);
+            //Shiba shiba = new Shiba();
+            //shiba.Info();
+            #endregion
+            #region 백준 - 단어길이 재기
+            string result = Console.ReadLine();
+            Console.WriteLine(result.Length);
+            #endregion
+        }
+    }
+}
--- ConsoleApp/boxingAndUnboxing/Program.cs
+++ ConsoleApp/boxingAndUnboxing/Program.cs
@@ -6,19 +6,24 @@ namespace boxingAndUnboxing
     {
         static void Main(string[] args)
         {
-            # region 박싱
-                        //값 형식 -> 참조형식으로 변환해주는 과정
-                        //암묵적 형변환이 발생
-                        //참조에 할당하는 것보다 20배 시간 소요
-                        int data = 100;
-                        object obj = data;
-                        //Console.WriteLine(obj);
+            #region 백준
+            //string s = Console.ReadLine();
+            //int i = int.Parse(Console.ReadLine());
+            //Console.WriteLine(s[i-1]);
             #endregion
-            # region 언박싱
+            #region 박싱
+            //값 형식 -> 참조형식으로 변환해주는 과정
+            //암묵적 형변환이 발생
+            //참조에 할당하는 것보다 20배 시간 소요
+            //int data = 100;
+            //object obj = data;
+            //Console.WriteLine(obj);
+            #endregion
+            #region 언박싱
             //박싱되어 있는 참조형식 -> 값형식으로 변환
             //명시적 형변환이 발생
             //메모리 할당하는 것보다 4배 소요
-            int value = (int)obj;
+            //int value = (int)obj;
             //Console.WriteLine(value);
             #endregion
             #region ArrayList
@@ -44,11 +49,11 @@ namespace boxingAndUnboxing
             //Console.WriteLine(name);
             #endregion
             #region 입력
-            string input = Console.ReadLine();
-            Console.WriteLine(input);
+            //string input = Console.ReadLine();
+            //Console.WriteLine(input);
 
-            int result = int.Parse(Console.ReadLine());
-            Console.WriteLine(result);
+            //int result = int.Parse(Console.ReadLine());
+            //Console.WriteLine(result);
             #endregion
         }
     }
