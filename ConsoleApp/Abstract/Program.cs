b3a0a034120cfe2c1615f8850a264022c2fcb3c4
이윤정 <saymeyjlee@gmail.com>
2023-03-26 03:26:06 +0900

    day9
    delegate

---
+++ ConsoleApp/Abstract/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Abstract
+{
+    abstract class Item
+    {
+        protected int price = 1000;
+
+        abstract public void Function();
+
+        abstract public void WaitTime();
+        public void Info()
+        {
+            Console.WriteLine("Item의 가격: " + price);
+        }
+
+    }
+    class Posion : Item
+    {
+        const string type = "포션류";//초기화 꼭 해야함 절대 바뀌지 않을 타입만 const
+        readonly int itemCode; //초기화 하지 않아도 됨 동적으로 바꿀 때는 readonly 단 속도가 느리다
+        public Posion()
+        {
+            itemCode = 2189;
+            Console.WriteLine("item Code : " + itemCode);
+        }
+        //메소드 숨기기 info를 덮어씌웠기 때문에 info()호출
+        new public void Info()
+        {
+            //base -> 부모 함수 호출
+            base.Info();
+            Console.WriteLine("포션 가격: " + price);
+        }
+        //미리 봉인하는 것
+        sealed public override void WaitTime()
+        {
+            Console.WriteLine("몇 초 대기");
+        }
+        public override void Function()
+        {
+            Console.WriteLine("체력 회복");
+        }
+    }
+    class HealthPosion : Posion
+    {
+        public override void Function()
+        {
+            Console.WriteLine("능력치 증가");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Item item = new Item();
+            #region 추상클래스
+            //Item item = new Posion();
+            //item.Function();
+            //item.Info();
+            #endregion
+            #region base
+            //Posion posion = new Posion();
+            //posion.Info();
+            #endregion
+            #region 문자열 보간
+            //문자열 보간
+            int year = 2023;
+            var date = DateTime.Now; //자동 자료형 맞춰줌
+            Console.WriteLine("{0}년 {1:HH:mm:ss:ms}", year, date);
+            //0에 year가 들어가고 1에 date가 들어가게된다
+            #endregion
+        }
+    }
+
+}
---
+++ ConsoleApp/Deligate/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Delegate
+{
+    delegate void Delegate();
+    class Player
+    {
+        public void Damage(Enemy enemy)
+        {
+            Delegate callback = State;
+            enemy.Attack(callback);
+        }
+        public void State()
+        {
+            Console.WriteLine("공격당함");
+        }
+    }
+    class Enemy
+    {
+        public void Attack(Delegate callback)
+        {
+            Console.WriteLine("attack");
+            callback();
+        }
+    }
+    //delegate - 대리자
+    //메소드를 대신 호출해줌
+    internal class Program
+    {
+        //델리게이트는 보관하기 위한 메소드와 반환형이 일치 해야함
+        //보관하기 위한 메소드의 매개변수도 일치해야함
+        delegate void Function(int x, int y);
+
+        static void Plus(int x, int y)
+        {
+            Console.WriteLine("x+y = " + (x+y));
+        }
+        static void Minus(int x, int y)
+        {
+            Console.WriteLine("x-y = " + (x-y));
+        }
+        static void Main(string[] args)
+        {
+            #region delegate
+            //Function function;
+            //function = Plus;//함수 주소값을 delegate에 넣음
+            //function(10, 20);
+
+            //function = Minus;
+            //function(10, 25);
+            //Plus(10, 20);
+            //Minus(10, 20);
+            #endregion
+            #region 델리게이트 체인
+            //Function function;
+
+            ////delegate 체인을 하려면 먼저 메소드를 등록시켜야함
+            //function = Plus;
+            //function += Minus;
+
+            //function(10, 35);
+
+            //function -= Minus;
+            //function(10, 35);
+            #endregion
+
+            Player player = new Player();
+            Enemy enemy = new Enemy();
+            //enemy.Attack(player.Damage);
+            player.Damage(enemy);
+        }
+    }
+}
